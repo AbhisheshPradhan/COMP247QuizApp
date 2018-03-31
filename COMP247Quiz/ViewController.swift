c197ce0fe68afbdb845770e0ac82444159013642
Abhishesh Pradhan <pradhan.abhishesh@gmail.com>
2018-04-01 02:02:59 +1100

    Initial Commit

---
+++ COMP247Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COMP247Quiz
+//
+//  Created by Abhishesh Pradhan on 1/4/18.
+//  Copyright © 2018 Abhishesh Pradhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
